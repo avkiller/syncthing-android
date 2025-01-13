94871d34d817c7c9796dccac06d239df4c95cf83
avkiller <avkiller2012@gmail.com>
2025-01-13 14:43:10 +0800

    Update build.gradle.kts
    更新sdk

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
     extra.apply {
         // Cannot be called "ndkVersion" as that leads to naming collision
         // Changes to this value must be reflected in `./docker/Dockerfile`
-        set("ndkVersionShared", "27.0.12077973")
+        set("ndkVersionShared", "27.2.12479018")
         set("versionMajor", 1)
         set("versionMinor", 29)
         set("versionPatch", 0)
