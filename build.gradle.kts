8128276b6787b1f59050a44cba87996b3462654b
avkiller <avkiller2012@gmail.com>
2025-03-13 21:03:01 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
         set("ndkVersionShared", "27.2.12479018")
         set("versionMajor", 1)
         set("versionMinor", 29)
-        set("versionPatch", 2)
+        set("versionPatch", 3)
         set("versionWrapper", 0)
     }
 
